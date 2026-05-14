960f6a98652db93d547caee451ab764dc27cc709
csmueed <csmueed@gmail.com>
2026-05-14 21:47:22 +0500

    Day 1 Completed - JavaScript

---
+++ 14May-Day1/dataTypes.js
@@ -0,0 +1,32 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log(3 
+    +
+     3) // code readability should be high
+
+console.log("Mueed")
+
+
+let name = "mueed"
+let age = 20
+let isLoggedIn = true;
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+// number, boolean, symbol, bigint, string, null, undefined.
+//object
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
