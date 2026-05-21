b9dfffe7d2273777b111834c59cc2d930302a91f
csmueed <csmueed@gmail.com>
2026-05-21 13:01:54 +0500

    learned more interesting things about functions

--- 21May-Day8/functions.js
+++ 21May-Day8/functions.js
@@ -3,12 +3,44 @@ function sayMyName(name) {
 }
 console.log(sayMyName("Mueed"));
 
-function addition (val1, val2) {
-    return console.log(val1 + val2);
+function addition(val1, val2) {
+  return console.log(val1 + val2);
 }
-addition(34,55);
+addition(34, 55);
 
-function greetingMessage (user = "Guest") {
-    return console.log((`${user} Just LoggedIN!`));
+function greetingMessage(user = "Guest") {
+  return console.log(`${user} Just LoggedIN!`);
 }
 greetingMessage("Mueed");
+
+function calculateCartPrices(...num) {
+  return console.log(num);
+}
+let myPrices = calculateCartPrices(488, 33, 560, 1080);
+
+const course = {
+  name: "Web Development",
+  instructor: "Hitesh Ch",
+  price: "PKR 2999",
+};
+
+function printGreetingMessage(Greets) {
+  return console.log(
+    `Hello, Welcome to the ${Greets.name} Course by ${Greets.instructor} in Just Offer of ${Greets.price}.`);
+};
+printGreetingMessage(course);
+
+const course2 = {
+    name: "Data Structure & Algorithms",
+    instructor: "Akshay Saini",
+    price: "PKR 8999"
+};
+printGreetingMessage(course2);
+
+const myArray = [10, 20, 30, 50];
+
+function returntheSecondValue (Arr) {
+    return console.log(`The Second Value of this Array is ${Arr[1]}`);
+};
+
+returntheSecondValue(myArray);
