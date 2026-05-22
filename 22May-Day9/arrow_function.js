4befbca8af1ee9832b9845492710693d9b7c3639
csmueed <csmueed@gmail.com>
2026-05-22 14:21:54 +0500

    learned about arrow function

---
+++ 22May-Day9/arrow_function.js
@@ -0,0 +1,19 @@
+// const printGreeting = (name,degree,uni) => {
+//     return console.log(`Hello, Mr. ${this.name}, Welcome to the degree program of BS ${degree}, at ${uni} University`);
+// };
+// printGreeting("Mueed", "Computer Sciences", "Virtual");
+
+const printGreeting = (name,degree,uni) => {
+    return console.log(`Hello, Mr. ${name}, Welcome to the degree program of BS ${degree}, at ${uni} University`);
+};
+printGreeting("Mueed", "Computer Sciences", "Virtual");
+
+const addTwoNumbers = (num1, num2) => {
+    return console.log(num1 + num2);
+}
+
+addTwoNumbers(7,8);
+
+const multiplyTwoNumbers = (num1, num2) => (console.log(num1 * num2));
+
+multiplyTwoNumbers(4,5);
