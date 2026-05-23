9e95ed8b9746594de74a0e03b7b636b2b4811d5d
csmueed <csmueed@gmail.com>
2026-05-23 22:27:51 +0500

    learned about ternary operator in js

---
+++ 23May-Day10/ternary-operator.js
@@ -0,0 +1,13 @@
+let age = 8;
+let result = age >= 18 ? "Voter" : "Minor";
+console.log(result);
+
+
+let marks = 55;
+let resultCard = marks >= 33 ? "Pass" : "Fail";
+console.log(resultCard);
+
+
+let isDark = false;
+let theme = isDark ? "Light" : "Dark";
+console.log(theme);
