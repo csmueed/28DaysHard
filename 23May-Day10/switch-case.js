4b5e5c6b6aafb936fb416a3c6074fca0d18acec7
csmueed <csmueed@gmail.com>
2026-05-23 22:18:39 +0500

    learned about switch case statement

---
+++ 23May-Day10/switch-case.js
@@ -0,0 +1,31 @@
+let Day = 2;
+switch (Day) {
+    case 1: console.log("Monday");
+        break;
+        case 2: console.log("Tuesday");
+    break;
+    case 3: console.log("Wednesday");
+    break;
+    default:
+        console.log("Other Days");
+        break;
+}
+
+
+let grades = "c".toUpperCase();
+switch (grades) {
+    case "A": console.log("Topper");
+    break;
+    case "B": console.log("Above Average");
+    break;
+    case "C": console.log("Average");
+    break;
+    case "D": console.log("Below Average");
+    break;
+    case "E": console.log("Poor");
+    break;
+    case "F": console.log("Fail");
+    default: console.log("Please Select Grade from A to F only.");
+    break;
+};
+
