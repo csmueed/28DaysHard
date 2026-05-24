790de80fdb690971d3258b447ffc7bd5264d99b1
csmueed <csmueed@gmail.com>
2026-05-24 14:15:32 +0500

    learned basics about for loops

---
+++ 24May-Day11/for-loop.js
@@ -0,0 +1,37 @@
+for (let i = 0; i <= 10; i++) {
+  //  console.log(i);
+}
+
+for (let i = 1; i <= 20; i++) {
+  // console.log(i);
+}
+
+for (let i = 1; i <= 10; i++) {
+  for (let j = 1; j <= 10; j++) {
+    // console.log(`${i} * ${j} = ${i * j}`);
+  }
+}
+
+for (let i = 0; i <= 3; i++) {
+  for (let j = 0; j <= 3; j++) {
+    for (let k = 0; k <= 3; k++) {
+    //   console.log(`Value of i is ${i}, j is ${j} and k is ${k}`);
+    }
+  }
+}
+
+for (let i = 10; i >= 0; i--){
+    // console.log(i);
+}
+
+for (let i = 0; i <= 50; i = i + 2){
+    // console.log(i);
+    
+}
+
+for (let i = 0; i <=50; i++){
+    if (i%2 != 0) {
+    //    console.log(i); 
+    }
+}
+
