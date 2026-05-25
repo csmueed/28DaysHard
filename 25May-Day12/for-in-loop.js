5dcd8a9bcf41ff1b13a9738f96294091486ab161
csmueed <csmueed@gmail.com>
2026-05-25 13:46:30 +0500

    for in loop

---
+++ 25May-Day12/for-in-loop.js
@@ -0,0 +1,10 @@
+const myLanguages = {
+  JavaScript: "Web Development",
+  Python: "Machine Learning",
+  Kotlin: "Android Development",
+  Swift: "IOS Development",
+};
+
+for (const key in myLanguages) {
+  console.log(`${key} is used in ${myLanguages[key]}`);
+};
