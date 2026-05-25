dfdbcb67c0b2cf52935bf92363536586cb115e8e
csmueed <csmueed@gmail.com>
2026-05-25 22:22:00 +0500

    map in js

---
+++ 25May-Day12/map.js
@@ -0,0 +1,10 @@
+let myArr = [1,2,3,4,5,6,7,8,9,10];
+let testedArr = myArr.map((item)=>{
+    return item + 2;
+});
+console.log(testedArr);
+
+let myName = ["ali", "hasan", "mueed"];
+let capitalName = myName.map((item) => item.toUpperCase());
+console.log(capitalName);
+
